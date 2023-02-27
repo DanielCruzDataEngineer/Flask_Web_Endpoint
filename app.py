6cedb018fb3de00072b6e0786239af397c78d055
DanielCruzDataEngineer <danielcruz.alu.lmb@gmail.com>
2023-02-27 11:21:34 -0300

    Data Changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 
 
@@ -9,7 +9,9 @@ GOOD_BOY_URL = (
     "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
     "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
 )
-
+@app.route("/home")
+def testes():
+    return render_template('index.html')
 
 @app.route("/whatsapp", methods=["GET", "POST"])
 def reply_whatsapp():
@@ -17,15 +19,16 @@ def reply_whatsapp():
     try:
         num_media = int(request.values.get("NumMedia"))
     except (ValueError, TypeError):
-        return "Invalid request: invalid or missing NumMedia parameter", 400
+        return render_template('index.html')
     response = MessagingResponse()
     if not num_media:
         msg = response.message("Helloooo")
+        
     else:
         msg = response.message("Thanks for the image. Here's one for you!")
         msg.media(GOOD_BOY_URL)
-    return str(response)
-
+    return render_template('index.html')
+    
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
